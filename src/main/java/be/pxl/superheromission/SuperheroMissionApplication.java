dd889f56d26c8dbee0f3c552b6d69f11a46ca744
HursitTarcan <HursitTarcan@gmail.com>
2022-08-15 18:17:34 +0200

    initial

---
+++ src/main/java/be/pxl/superheromission/SuperheroMissionApplication.java
@@ -0,0 +1,13 @@
+package be.pxl.superheromission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperheroMissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperheroMissionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/pxl/superheromission/SuperheroMissionApplicationTests.java
@@ -0,0 +1,13 @@
+package be.pxl.superheromission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperheroMissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
